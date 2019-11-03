abf5d285a8049988380a596867e6b5d28e91f559
Fabio Buso <fabio@logicalclocks.com>
2019-11-03 13:37:29 +0100

    Prepare for 1.1.0-SNAPSHOT development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer              'Logical Clocks AB'
 maintainer_email        'fabio@logicalclocks.com'
 license                 ''
 description             'Installs/Configures kube-hops-chef'
-version                 '1.0.0'
+version                 '1.1.0'
 
 depends 'kagent'
 depends 'ndb'
