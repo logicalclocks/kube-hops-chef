f16fc3a6e5e4e69b90c9b22c8e9c90305ed74eed
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2020-04-07 14:48:49 +0200

    [HOPSWORKS-1405] Register API service with Consul and do not touch /etc/resolv.conf (#4)

--- recipes/post_conf.rb
+++ recipes/post_conf.rb
@@ -1,18 +1,3 @@
-# Change machine DNS to use the Kubernetes internal one
-# It will fall back to the specified dns server if you look for logicalclocks.com
-
-# Make a backup before overwriting it.
-remote_file "/etc/resolv.conf.bck" do 
-  owner "root"
-  group "root"
-  source "file:///etc/resolv.conf"
-  mode "644"
-  action :create_if_missing
-end
-
-node.override['resolver']['nameservers'] = ["#{node['kube-hops']['dns_ip']}"]
-include_recipe "resolver::default"
-
 # Add ca.crt to /etc/docker/cert.d/docker-regstry for docker
 # to be able to pull images from the private registry
 directory '/etc/docker/certs.d/' do
