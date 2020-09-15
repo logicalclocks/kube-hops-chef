ca71e9e1804298d7f7b3aab6f20c2f0151bd67dc
Jim Dowling <jdowling@sics.se>
2020-09-15 11:09:47 +0200

    versions

--- metadata.rb
+++ metadata.rb
@@ -18,7 +18,7 @@ recipe 'kube-hops::master', 'Configure a node as Kubernetes master'
 recipe 'kube-hops::node', 'Configure a node as Kubernetes slave'
 recipe 'kube-hops::addons', 'Deploy addons on the cluster'
 recipe 'kube-hops::hopsworks', 'Configure Hopsworks to use Kubernetes'
-
+recipe 'kube-hops::kfserving', 'Configure and install KFServing (helm, istio, knative) on Kubernetes'
 
 attribute "kube-hops/device",
           :description =>  "Device plugin to configure for master and nodes, for no device set '' or 'nvidia' for NVIDIA GPUs",
