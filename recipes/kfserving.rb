2d826b32b913c9822ddfdb168848873c4757cc31
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2020-09-23 23:43:27 +0200

    Fix kfserving recipe. Include templates. (#28)

--- recipes/kfserving.rb
+++ recipes/kfserving.rb
@@ -1,223 +1,110 @@
-# Install KFServing and required tools
+  # Install KFServing and required tools
 
-private_ip = my_private_ip()
+  # Istio
 
-# Helm
+  bash 'install-istioctl' do
+    user node['kube-hops']['user']
+    group node['kube-hops']['group']
+    environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })
+    cwd ::Dir.home(node['kube-hops']['user'])
+    code <<-EOH
+      wget -nc #{node['kube-hops']['istio_url']}
+      tar zxf istio-#{node['kube-hops']['istio_version']}-linux-amd64.tar.gz
+      rm -f istio
+      ln -s istio-#{node['kube-hops']['istio_version']} istio
+      EOH
+    not_if { File.exist? "/home/#{node['kube-hops']['user']}/istio-#{node['kube-hops']['istio_version']}/bin/istioctl" }
+  end
 
-# The script detects arch/os and install helm accordingly
-# Optional args:
-# -v | --version: define specific version
-# --no-sudo: install without sudo
-template "/home/#{node['kube-hops']['user']}/istio.yml" do
-  source "istio.yml.erb"
-  owner node['kube-hops']['user']
-  group node['kube-hops']['group']
-end
+  template "/home/#{node['kube-hops']['user']}/istio-operator.yaml" do
+    source "istio-operator.yml.erb"
+    owner node['kube-hops']['user']
+    group node['kube-hops']['group']
+  end
 
-template "/home/#{node['kube-hops']['user']}/istio-configure.yml" do
-  source "istio-configure.yml.erb"
-  owner node['kube-hops']['user']
-  group node['kube-hops']['group']
-end
+  bash 'apply-istio' do
+    user node['kube-hops']['user']
+    group node['kube-hops']['group']
+    environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })
+    cwd ::Dir.home(node['kube-hops']['user'])
+    code <<-EOH
+      istio/bin/istioctl install -f istio-operator.yaml
+      EOH
+  end
 
-template "/home/#{node['kube-hops']['user']}/knative-configure.yml" do
-  source "knative-configure.yml.erb"
-  owner node['kube-hops']['user']
-  group node['kube-hops']['group']
-end
+  # Knative
 
-template "/home/#{node['kube-hops']['user']}/certmgr.yml" do
-  source "certmgr.yml.erb"
-  owner node['kube-hops']['user']
-  group node['kube-hops']['group']
-end
+  template "/home/#{node['kube-hops']['user']}/knative-serving-crds.yaml" do
+    source "knative-serving-crds.yml.erb"
+    owner node['kube-hops']['user']
+    group node['kube-hops']['group']
+  end
 
-template "/home/#{node['kube-hops']['user']}/cert-manager.crds.yaml" do
-  source "cert-manager.crds.yaml.erb"
-  owner node['kube-hops']['user']
-  group node['kube-hops']['group']
-end
+  template "/home/#{node['kube-hops']['user']}/knative-serving.yaml" do
+    source "knative-serving.yml.erb"
+    owner node['kube-hops']['user']
+    group node['kube-hops']['group']
+  end
 
-template "/home/#{node['kube-hops']['user']}/kfserving.yml" do
-  source "kfserving.yml.erb"
-  owner node['kube-hops']['user']
-  group node['kube-hops']['group']
-end
+  template "/home/#{node['kube-hops']['user']}/knative-istio.yaml" do
+    source "knative-istio.yml.erb"
+    owner node['kube-hops']['user']
+    group node['kube-hops']['group']
+  end
 
+  kube_hops_kubectl 'apply-knative-serving-crds' do
+    user node['kube-hops']['user']
+    group node['kube-hops']['group']
+    url "/home/#{node['kube-hops']['user']}/knative-serving-crds.yaml"
+  end
 
-bash 'install-helm' do
-  user 'root'
-  group 'root'
-  code <<-EOH
-    cd "#{Chef::Config['file_cache_path']}"
-    export PATH=$PATH:/usr/local/bin
-    curl -fsSL -o get_helm.sh #{node['kube-hops']['helm_script_url']}
-    chmod 700 get_helm.sh
-    ./get_helm.sh
-    EOH
-end
+  kube_hops_kubectl 'apply-knative-serving' do
+    user node['kube-hops']['user']
+    group node['kube-hops']['group']
+    url "/home/#{node['kube-hops']['user']}/knative-serving.yaml"
+  end
 
-bash 'configure-helm' do
-  user node['kube-hops']['user']
-  group node['kube-hops']['group']
-  environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })    
-#  ignore_failure true
-  code <<-EOH
-    cd /home/#{node['kube-hops']['user']}
-    export PATH=$PATH:/usr/local/bin
-    chmod 400 .kube/config
-    helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-    helm repo add jetstack https://charts.jetstack.io # cert-manager
-    helm repo update
-    EOH
-end
+  kube_hops_kubectl 'apply-knative-istio-controller' do
+    user node['kube-hops']['user']
+    group node['kube-hops']['group']
+    url "/home/#{node['kube-hops']['user']}/knative-istio.yaml"
+  end
 
-# Istio
+  # Cert-manager
 
-# Download binaries
-bash 'install-istio1' do
-  user node['kube-hops']['user']
-  group node['kube-hops']['group']
-  environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })  
-  code <<-EOH
-    cd /home/#{node['kube-hops']['user']}
-    wget -nc #{node['kube-hops']['istio_url']}
-    tar zxf istio-#{node['kube-hops']['istio_version']}-linux-amd64.tar.gz
-    rm -f $HOME/istio
-    ln -s $HOME/istio-#{node['kube-hops']['istio_version']} $HOME/istio
-    EOH
-  not_if { File.exist? "/home/#{node['kube-hops']['user']}/istio-#{node['kube-hops']['istio_version']}/bin/istioctl" }
-end
+  template "/home/#{node['kube-hops']['user']}/cert-manager.yaml" do
+    source "cert-manager.yml.erb"
+    owner node['kube-hops']['user']
+    group node['kube-hops']['group']
+  end
 
-#    port: <%= node['kube-hops']['apiserver']['port'] %>
-# Install CRDs
-bash 'install-istio2' do
-  user node['kube-hops']['user']
-  group node['kube-hops']['group']
-  environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })    
-  code <<-EOH
-    cd /home/#{node['kube-hops']['user']}
-    export PATH=$PATH:/usr/local/bin:$HOME/istio/bin
-    export KUBECONFIG=$KUBECONFIG:/home/#{node['kube-hops']['user']}/.kube/config
-    kubectl apply -f istio.yml
-    EOH
-end
+  kube_hops_kubectl 'apply-cert-manager' do
+    user node['kube-hops']['user']
+    group node['kube-hops']['group']
+    flags "--validate=false"
+    url "/home/#{node['kube-hops']['user']}/cert-manager.yaml"
+  end
 
-bash 'install-istio3' do
-  user node['kube-hops']['user']
-  group node['kube-hops']['group']
-  environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })      
-  code <<-EOH
-    cd /home/#{node['kube-hops']['user']}
-    export PATH=$PATH:/usr/local/bin:$HOME/istio/bin
-    export KUBECONFIG=$KUBECONFIG:/home/#{node['kube-hops']['user']}/.kube/config
+  bash 'wait-for-cert-manager' do
+    user node['kube-hops']['user']
+    group node['kube-hops']['group']
+    environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })
+    code <<-EOH
+      kubectl wait --for=condition=Ready pods --all -n cert-manager --timeout=60s
+      sleep 15s
+      EOH
+  end
 
-    helm template istio-#{node['kube-hops']['istio_version']}/manifests/charts/istio-operator/ \
-      --kube-apiserver https://#{private_ip}:#{node['kube-hops']['apiserver']['port']} \
-      --insecure-skip-tls-verify \
-      --set hub=docker.io/istio \
-      --set tag=#{node['kube-hops']['istio_version']} \
-      --set operatorNamespace=istio-operator \
-      --set watchedNamespaces=istio-system > kube-helm.yml
-    kubectl apply -f kube-helm.yml
-    EOH
-end
+  # KFServing
 
+  template "/home/#{node['kube-hops']['user']}/kfserving.yaml" do
+    source "kfserving.yml.erb"
+    owner node['kube-hops']['user']
+    group node['kube-hops']['group']
+  end
 
-# Install Istio Operator: without sidecar injection
-# Notes: targetPort cannot be lower than 1024.
-#        istioctl manifest apply not known, use install to apply manifests
-bash 'configure-istio' do
-  user node['kube-hops']['user']
-  group node['kube-hops']['group']
-  environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })      
-  code <<-EOH
-    cd /home/#{node['kube-hops']['user']}
-    chmod 400 $HOME/.kube/config
-    export PATH=$PATH:/usr/local/bin:$HOME/istio/bin
-    export KUBECONFIG=$KUBECONFIG:/home/#{node['kube-hops']['user']}/.kube/config
-    istioctl manifest install -f istio-configure.yml
-    EOH
-#  not_if ""
-end
-
-#
-# Knative
-#
-
-# Install Knative Operator
-# Note: Only knative serving is required. (Knative eventing can be installed independently)
-bash 'install-knative' do
-  user node['kube-hops']['user']
-  group node['kube-hops']['group']
-  environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })      
-  code <<-EOH
-    cd /home/#{node['kube-hops']['user']}
-    export PATH=$PATH:/usr/local/bin:$HOME/istio/bin
-    export KUBECONFIG=$KUBECONFIG:/home/#{node['kube-hops']['user']}/.kube/config
-    kubectl apply -f #{node['kube-hops']['knative_chart']}
-    EOH
-#  not_if ""
-end
-
-# Install Knative Serving component
-bash 'configure-knative' do
-  user node['kube-hops']['user']
-  group node['kube-hops']['group']
-  environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })      
-  code <<-EOH
-    cd /home/#{node['kube-hops']['user']}
-    export PATH=$PATH:/usr/local/bin:$HOME/istio/bin
-    export KUBECONFIG=$KUBECONFIG:/home/#{node['kube-hops']['user']}/.kube/config
-    kubectl apply -f knative-configure.yml
-    EOH
-end
-
-#
-# Cert-manager
-#
-
-# Install Cert-manager
-bash 'install-cert-manager' do
-  user node['kube-hops']['user']
-  group node['kube-hops']['group']
-  environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })      
-  code <<-EOH
-    cd /home/#{node['kube-hops']['user']}
-    export KUBECONFIG=$KUBECONFIG:/home/#{node['kube-hops']['user']}/.kube/config
-    export PATH=$PATH:/usr/local/bin:$HOME/istio/bin
-    kubectl apply -f certmgr.yml
-    kubectl apply -f cert-manager.crds.yaml
-    EOH
-end
-
-# bash 'helm-cert-manager' do
-#   user node['kube-hops']['user']
-#   group node['kube-hops']['group']
-#   environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })      
-#   code <<-EOH
-#     cd /home/#{node['kube-hops']['user']}
-#     export KUBECONFIG=$KUBECONFIG:/home/#{node['kube-hops']['user']}/.kube/config
-#     export PATH=$PATH:/usr/local/bin:$HOME/istio/bin
-
-#     helm install cert-manager jetstack/cert-manager --namespace cert-manager \
-#       --version v#{node['kube-hops']['certmgr_version']} --set installCRDs=true
-#     EOH
-# end
-
-#
-# KFServing
-#
-
-# Note: KFServing v0.4.0 not supported with Kubernetes 1.18
-bash 'install-kfserving' do
-  user node['kube-hops']['user']
-  group node['kube-hops']['group']
-  environment ({ 'HOME' => ::Dir.home(node['kube-hops']['user']) })      
-  code <<-EOH
-    export KUBECONFIG=$KUBECONFIG:/home/#{node['kube-hops']['user']}/.kube/config
-    cd /home/#{node['kube-hops']['user']}
-    export PATH=$PATH:/usr/local/bin:$HOME/istio/bin
-    kubectl apply -f kfserving.yaml
-    EOH
-end
+  kube_hops_kubectl 'apply-kfseving' do
+    user node['kube-hops']['user']
+    group node['kube-hops']['group']
+    url "/home/#{node['kube-hops']['user']}/kfserving.yaml"
+  end
