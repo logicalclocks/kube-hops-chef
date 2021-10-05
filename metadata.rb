532f2223cf8b5eeddfe5ef91e5c39383f6dcabba
tkakantousis <t.kakantousis@gmail.com>
2021-10-05 09:00:16 +0200

    Update version to: 2.4.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer              'Logical Clocks AB'
 maintainer_email        'fabio@logicalclocks.com'
 license                 ''
 description             'Installs/Configures kube-hops-chef'
-version                 '3.0.0'
+version                 '2.4.0'
 
 cookbook 'sysctl', '~> 1.0.3'
 cookbook 'kernel_module', '~> 1.1.1'
