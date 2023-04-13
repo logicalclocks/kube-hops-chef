25aac1379890af8e0b757d4be5fe272cc80408c3
Fabio Buso <fabio@logicalclocks.com>
2023-04-13 11:39:00 +0200

    Prepare development for: 3.3.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer              'Logical Clocks AB'
 maintainer_email        'fabio@logicalclocks.com'
 license                 ''
 description             'Installs/Configures kube-hops-chef'
-version                 '3.2.0'
+version                 '3.3.0'
 
 cookbook 'sysctl', '~> 1.0.3'
 cookbook 'kernel_module', '~> 1.1.1'
